cdb52803c688f2c1b84da1e4a93f5e3e50628005
artem-il2004 <artemilchenko28@gmail.com>
2025-04-21 11:47:40 +0200

    Update auth.js

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -25,25 +25,15 @@ export const loginUserController = async (req, res) => {
   });
 };
 export const registerController = async (req, res) => {
-  const session = await registerUser(req.body); 
+  const user = await registerUser(req.body);
 
-  res.cookie('refreshToken', session.refreshToken, {
-    httpOnly: true,
-    expires: new Date(Date.now() + ONE_DAY),
-  });
-  res.cookie('sessionId', session._id, {
-    httpOnly: true,
-    expires: new Date(Date.now() + ONE_DAY),
-  });
-
-  res.json({
-    status: 200,
+  res.status(201).json({
+    status: 201,
     message: 'Successfully registered a user!',
-    data: {
-      accessToken: session.accessToken,
-    },
+    data: user,
   });
 };
+
 
 
 export const logoutUserController = async (req, res) => {
